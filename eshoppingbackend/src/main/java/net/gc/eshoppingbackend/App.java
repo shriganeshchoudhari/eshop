4c3cbe1bde7447003e054939207e7ca39903ee6f
shriganesh <shriganesha0@gmail.com>
2018-08-09 13:03:16 +0530

    Start setup Of Project done

---
+++ eshoppingbackend/src/main/java/net/gc/eshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.gc.eshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
